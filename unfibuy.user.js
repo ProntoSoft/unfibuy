467318ac595e711fc940abf1aaa46d82991d151a
Sam Stavinoha <smlstvnh@gmail.com>
2020-04-24 16:19:10 -0500

    if no delimiter, price is for quantity shown

--- unfibuy.user.js
+++ unfibuy.user.js
@@ -43,7 +43,7 @@
 function parsePackSize(packSize = '') {
   const parsePackSizeRe = /([\d]+[\s]?)([xX\/]?[\s]?)([+?-?\d+(\.\d+)?$]*[\s]?)([\w\#]*)/g;
   const groups = parsePackSizeRe.exec(packSize);
-
+  console.log(groups)
   return {
     quantity: groups[1],
     measure: groups[3],
@@ -73,9 +73,23 @@ function parseTotalPrice(totalPrice = '') {
 function getPricePerUnit(packSize, totalPrice) {
   const {measure, quantity, unit} = parsePackSize(packSize);
   const {amount, currencyCode} = parseTotalPrice(totalPrice);
-  const pricePerUnitCalculation = (amount / quantity / measure).toFixed(2);
-  const message = `${currencyCode}${pricePerUnitCalculation} per ${unit}`;
+  // sometimes, there is only 1 package, implied by no measure, no delimiter
+  let pricePerUnitCalculation;
+  if (measure != "") {
+    pricePerUnitCalculation = (amount / quantity / measure).toFixed(3);
+  } else {
+    pricePerUnitCalculation = (amount / quantity).toFixed(3);
+  }
 
+  let message = `${currencyCode}${pricePerUnitCalculation} per ${unit}`;
+  switch (unit) {
+    case "lb":
+    case "LB":
+    case "#":
+      var pricePerOunce = (pricePerUnitCalculation / 16).toFixed(3)
+      message += ` | ${currencyCode}${pricePerOunce} per oz`;
+      break;
+  }
   console.log('[UNFIBUY]: parsed price per unit:', message);
 
   return message;
